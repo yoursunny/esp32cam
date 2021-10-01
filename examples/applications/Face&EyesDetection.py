da6cadf285332c4adfd405b16474ce80e0bac1a9
dolendravikas <8112001@gmail.com>
2021-10-01 11:33:26 +0530

    formatted the code in pep8 fromat

--- examples/applications/Face&EyesDetection.py
+++ examples/applications/Face&EyesDetection.py
@@ -17,35 +17,36 @@ import cv2
 import urllib.request
 import numpy as np
 import sys
- 
-f_cas= cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
-eye_cascade=cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_eye.xml')
- 
-url='http://'
+
+f_cas = cv2.CascadeClassifier(
+    cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + 'haarcascade_eye.xml')
+
+url = 'http://'
 url = url + sys.argv[1]
 url = url + '/cam-lo.jpg'
 
 ##'''cam.bmp / cam-lo.jpg /cam-hi.jpg / cam.mjpeg '''
 cv2.namedWindow("Live Transmission", cv2.WINDOW_AUTOSIZE)
- 
+
 while True:
-    img_resp=urllib.request.urlopen(url)
-    imgnp=np.array(bytearray(img_resp.read()),dtype=np.uint8)
-    img=cv2.imdecode(imgnp,-1)
-    gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    face=f_cas.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=5)
-    for x,y,w,h in face:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),3)
+    img_resp = urllib.request.urlopen(url)
+    imgnp = np.array(bytearray(img_resp.read()), dtype=np.uint8)
+    img = cv2.imdecode(imgnp, -1)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    face = f_cas.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+    for x, y, w, h in face:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 3)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
         eyes = eye_cascade.detectMultiScale(roi_gray)
-        for (ex,ey,ew,eh) in eyes:
-            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
- 
- 
-    cv2.imshow("live transmission",img)
-    key=cv2.waitKey(5)
-    if key==ord('q'):
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+
+    cv2.imshow("live transmission", img)
+    key = cv2.waitKey(5)
+    if key == ord('q'):
         break
- 
+
 cv2.destroyAllWindows()
