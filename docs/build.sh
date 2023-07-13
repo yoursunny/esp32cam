476f0ebead880debc1abb659cb9fbcf6a6f00b71
Junxiao Shi <git@mail1.yoursunny.com>
2023-07-12 23:59:37 -0400

    docs: Google Analytics 4

--- docs/build.sh
+++ docs/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-set -eo pipefail
+set -euo pipefail
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 doxygen Doxyfile 2>&1 | ./filter-Doxygen-warning.awk 1>&2
 
-find html -name '*.html' | xargs sed -i '/<\/head>/ i\ <script>(function(e,t,n,i,s,a,c){e[n]=e[n]||function(){(e[n].q=e[n].q||[]).push(arguments)};a=t.createElement(i);c=t.getElementsByTagName(i)[0];a.async=true;a.src=s;c.parentNode.insertBefore(a,c)})(window,document,"galite","script","https://cdn.jsdelivr.net/npm/ga-lite@2/dist/ga-lite.min.js"); if (location.hostname.endsWith(".yoursunny.dev")) { galite("create", "UA-935676-14", "auto"); galite("send", "pageview"); }</script>'
+find html -name '*.html' | xargs sed -i '/<\/head>/ i\<script async src="https://www.googletagmanager.com/gtag/js?id=G-YSW3MP43Z4"></script><script>window.dataLayer=[];function gtag(){dataLayer.push(arguments);}if(location.hostname.endsWith(".yoursunny.dev")){gtag("js",new Date());gtag("config","G-MLZ5G2C4X2");}</script></script>'
 cp _redirects html/
